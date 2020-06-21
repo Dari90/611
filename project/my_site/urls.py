0bfca9b2f3308d4222b19164d75839a6dbd4515f
Dari <veonnika39@gmail.com>
2020-06-21 18:35:33 +0400

    611

---
+++ project/my_site/urls.py
@@ -0,0 +1,35 @@
+"""my_site URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from p_library import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('index/book_increment/', views.book_increment),
+    path('index/book_decrement/', views.book_decrement),
+    path('redaction_list/', views.redaction_list),
+    path('friend_list/', views.friend_list),
+    path('library/', views.library),
+    path('p/', include('p_library.urls', namespace='p_library')),
+]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
---
+++ project/p_library/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+from p_library.models import Book, Author, Friend, Publishing_house
+from p_library.forms import FriendForm
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from django.forms import formset_factory  
+from django.http.response import HttpResponseRedirect
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.utils import timezone
+
+def index(request):
+    template = loader.get_template('base.html')
+    books = Book.objects.all()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books": books,
+    }
+    return HttpResponse(template.render(biblio_data, request))
+
+def library(request):
+    template = loader.get_template('index.html')
+    books = Book.objects.all()
+    biblio_data = {
+        "books": books,
+    }
+    return HttpResponse(template.render(biblio_data, request))
+
+def redaction_list(request):
+    template = loader.get_template('redaction_list.html')
+    redactions = Publishing_house.objects.all()
+    publishers_data = {"data": {publishing_hous.name: 
+						list(Book.objects.filter(publishing_house=publishing_hous.id).values_list("title", flat=True))
+						 for publishing_hous in redactions}}
+    return HttpResponse(template.render(publishers_data, request))
+
+def friend_list(request):
+	template = loader.get_template('friend_list.html')
+	friends = Friend.objects.all()
+	data = {"friends": friends,}
+	return HttpResponse(template.render(data, request))	
+    
+class FriendList(ListView):
+	model = Friend
+	template_name = 'friend_form.html'
+
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            book.copy_count += 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+        
+
+
